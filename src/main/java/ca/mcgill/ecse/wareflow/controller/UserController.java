ab33a1f92af2bdbe05f8673fcb78c3898de546a4
yvehenry <yvehenry.julsain@mail.mcgill.ca>
2024-03-12 13:19:13 -0400

    finished methods in the user controller file

--- src/main/java/ca/mcgill/ecse/wareflow/controller/UserController.java
+++ src/main/java/ca/mcgill/ecse/wareflow/controller/UserController.java
@@ -5,25 +5,25 @@ import ca.mcgill.ecse.wareflow.model.*;
 
 public class UserController {
 
-    /**
-     * @author Yvehenry Julsain
-     * This method is used to update the manager's password within the WareFlow application.
-     * @param password the new password of the manager's account.
-     * @return Returns if the password has been successfully updated.
-     */
-    public static String updateManager(String password) {
-        WareFlow wareFlow = WareFlowApplication.getWareFlow();
-        Manager manager = wareFlow.getManager();
-        manager.setPassword(password);
-        return "Password successfully updated!";
-    }
+/**
+ * @author Yvehenry Julsain
+ * This method is used to update the manager's password within the WareFlow application.
+ * @param password the new password of the manager's account.
+ * @return Returns if the password has been successfully updated.
+ */
+public static String updateManager(String password) {
+    WareFlow wareFlow = WareFlowApplication.getWareFlow();
+    Manager manager = wareFlow.getManager();
+    manager.setPassword(password);
+    return "Password successfully updated!";
+}
 
-    // address is ignored if the isEmployee is true
-    public static String addEmployeeOrClient(String username, String password, String name,
-                                             String phoneNumber, boolean isEmployee, String address) {
+// address is ignored if the isEmployee is true
+public static String addEmployeeOrClient(String username, String password, String name,
+                                         String phoneNumber, boolean isEmployee, String address) {
 
-        throw new UnsupportedOperationException("Not Implemented!");
-    }
+    throw new UnsupportedOperationException("Not Implemented!");
+}
 
     // newAddress is ignored if the user is an employee
     public static String updateEmployeeOrClient(String username, String newPassword, String newName,
