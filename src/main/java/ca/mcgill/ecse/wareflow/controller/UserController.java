dd779abe890c94ab4a6c205a17c50bd3f3400997
yvehenry <yvehenry.julsain@mail.mcgill.ca>
2024-03-12 13:19:13 -0400

    finished methods in the user controller file

--- src/main/java/ca/mcgill/ecse/wareflow/controller/UserController.java
+++ src/main/java/ca/mcgill/ecse/wareflow/controller/UserController.java
@@ -15,12 +15,29 @@ public class UserController {
     manager.setPassword(password);
     return "Password sucessfully updated!";
   }
-
+/**
+   * @author Yvehenry Julsain
+   * This method is used to add an employee or a client within the WareFlow application.
+   * @param username
+   * @param password
+   * @param name
+   * @param phoneNumber
+   * @param isEmployee
+   * @param address
+   * @return Returns if an employee or a client sucessfully been added to the WareFlow Application.
+   */
   // address is ignored if the isEmployee is true
-  public static String addEmployeeOrClient(String username, String password, String name,
-      String phoneNumber, boolean isEmployee, String address) { //TODO
-    
-    throw new UnsupportedOperationException("Not Implemented!");
+  public static String addEmployeeOrClient(String username, String password, String name, String phoneNumber, boolean isEmployee, String address) { //TODO
+    WareFlow wareFlow = WareFlowApplication.getWareFlow();
+    if (isEmployee) {
+      //Employee nEmployee = new Employee(username, name, password, phoneNumber, wareFlow);
+      wareFlow.addEmployee(username, name, password, phoneNumber);
+      return "The employee has been sucessfully added!";
+    }
+    else {
+      wareFlow.addClient(username, name, password, phoneNumber, address);
+      return "The client has been sucessfully added!";
+    }
   }
 
   // newAddress is ignored if the user is an employee
