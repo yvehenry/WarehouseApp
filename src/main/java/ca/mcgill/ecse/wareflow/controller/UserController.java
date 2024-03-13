83ca8fb66c82c355866f303e25ef20ffef100f9a
Al-Faysal Haidar <al.haidar@mail.mcgill.ca>
2024-03-12 22:20:04 -0400

    Resolved conflicts

--- src/main/java/ca/mcgill/ecse/wareflow/controller/UserController.java
+++ src/main/java/ca/mcgill/ecse/wareflow/controller/UserController.java
@@ -1,25 +1,34 @@
 package ca.mcgill.ecse.wareflow.controller;
 
-import ca.mcgill.ecse.wareflow.model.*;
 import ca.mcgill.ecse.wareflow.application.WareFlowApplication;
+import ca.mcgill.ecse.wareflow.model.*;
 
 public class UserController {
+
+    /**
+     * @author Yvehenry Julsain
+     * This method is used to update the manager's password within the WareFlow application.
+     * @param password the new password of the manager's account.
+     * @return Returns if the password has been successfully updated.
+     */
     public static String updateManager(String password) {
-        // Remove this exception when you implement this method
-        throw new UnsupportedOperationException("Not Implemented!");
+        WareFlow wareFlow = WareFlowApplication.getWareFlow();
+        Manager manager = wareFlow.getManager();
+        manager.setPassword(password);
+        return "Password successfully updated!";
     }
 
     // address is ignored if the isEmployee is true
     public static String addEmployeeOrClient(String username, String password, String name,
                                              String phoneNumber, boolean isEmployee, String address) {
-        // Remove this exception when you implement this method.
+
         throw new UnsupportedOperationException("Not Implemented!");
     }
 
     // newAddress is ignored if the user is an employee
     public static String updateEmployeeOrClient(String username, String newPassword, String newName,
                                                 String newPhoneNumber, String newAddress) {
-        // Remove this exception when you implement this method
+
         throw new UnsupportedOperationException("Not Implemented!");
     }
 
