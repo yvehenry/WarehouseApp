6188ab39d1ed2625f01bfafa800cd966508c679b
Al-Faysal Haidar <al.haidar@mail.mcgill.ca>
2024-03-12 22:41:18 -0400

    Cleaned up code

--- src/main/java/ca/mcgill/ecse/wareflow/controller/UserController.java
+++ src/main/java/ca/mcgill/ecse/wareflow/controller/UserController.java
@@ -10,22 +10,18 @@ public class UserController {
 
     /**
      * @param password the new password of the manager's account.
-     * @return Returns if the password has been sucessfully updated.
+     * @return Returns if the password has been successfully updated.
      * @author Yvehenry Julsain
      * This method is used to update the manager's password within the WareFlow application.
      */
 
-    public static String updateManager(String password) { //TODO
+    public static String updateManager(String password) {
         Manager manager = wareFlow.getManager();
         String message = "";
-    /*if (self.username != "manager") { //
-      return "Username has to be manager.";
-    }*/
-
-        if (password == "" || password == null) {
+        if (password.isEmpty()) {
             message += "The password must not be empty. ";
         }
-        if (!(password == "" || password == null)) {
+        if (!(password.isEmpty())) {
             if (password.length() < 4) {
                 message += "Password must be at least four characters long. ";
             }
@@ -57,7 +53,7 @@ public class UserController {
 
         if (message.isEmpty()) {
             manager.setPassword(password);
-            return "Password sucessfully updated!";
+            return "Password successfully updated!";
         }
         return message;
     }
@@ -68,40 +64,40 @@ public class UserController {
      * @param password    the password associated to the new employee or client account
      * @param name        the name associated to the new employee or client account
      * @param phoneNumber the phone number associated to the new employee or client account
-     * @param isEmployee  if the the account is an employee or a client account; parameter is true if the account created is an employee account
+     * @param isEmployee  if the account is an employee or a client account; parameter is true if the account created is an employee account
      * @param address     if the account is a client account, the address associated to the new client account
-     * @return Returns if an employee or a client sucessfully been added to the WareFlow Application.
+     * @return Returns if an employee or a client successfully been added to the WareFlow Application.
      * @author Yvehenry Julsain
      * This method is used to add an employee or a client within the WareFlow application.
      */
 
     public static String addEmployeeOrClient(String username, String password, String name, String phoneNumber, boolean isEmployee, String address) { //TODO
 
-        String message = "";
-        if (username == "manager") {
-            message += "The username cannot be 'manager'. ";
+        StringBuilder message = new StringBuilder();
+        if (username.equals("manager")) {
+            message.append("The username cannot be 'manager'. ");
         }
-        if (username == "" || username == null) {
-            message += "The username must not be empty. ";
+        if (username.isEmpty()) {
+            message.append("The username must not be empty. ");
         }
-        if (!(username == null || username.isEmpty())) {
+        if (!username.isEmpty()) {
             char[] usernameArray = username.toCharArray();
             for (char letter : usernameArray) {
                 if (!("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890 ".contains(String.valueOf(letter)))) {
-                    message += "The username contains an invalid character. The username can only contain letters from a to z, letters from A to Z, and numbers from 0 to 9. ";
+                    message.append("The username contains an invalid character. The username can only contain letters from a to z, letters from A to Z, and numbers from 0 to 9. ");
                 }
             }
         }
 
-        if (password == "" || password == null) {
-            message += "The password must not be empty. ";
+        if (password.isEmpty()) {
+            message.append("The password must not be empty. ");
         }
-        if (!(password == "" || password == null)) {
+        if (!password.isEmpty()) {
             if (password.length() < 4) {
-                message += "Password must be at least four characters long. ";
+                message.append("Password must be at least four characters long. ");
             }
             if (!(password.contains("!") || password.contains("#") || password.contains("$"))) {
-                message += "Password must contain a special character out of <!#$> ";
+                message.append("Password must contain a special character out of <!#$> ");
             }
             char[] passwordArray = password.toCharArray();
             boolean containsUppercase = false;
@@ -119,32 +115,32 @@ public class UserController {
                 }
             }
             if (!containsUppercase) {
-                message += "Password must contain an upper case character. ";
+                message.append("Password must contain an upper case character. ");
             }
             if (!containsLowercase) {
-                message += "Password must contain an lower case character. ";
+                message.append("Password must contain an lower case character. ");
             }
         }
 
         if (name == null) {
-            message += "The name must not be null. ";
+            message.append("The name must not be null. ");
         }
-        if (phoneNumber == "" || phoneNumber == null) {
-            message += "The phone number must not be empty. ";
+        if (phoneNumber.isEmpty()) {
+            message.append("The phone number must not be empty. ");
         }
 
         List<Employee> employees = wareFlow.getEmployees();
         for (Employee employee : employees) {
-            if (username == "manager" || username == "" || username == null) break;
-            if (employee.getUsername() == username) {
-                message += "This username is already taken. ";
+            if (username.equals("manager") || username.isEmpty()) break;
+            if (employee.getUsername().equals(username)) {
+                message.append("This username is already taken. ");
                 break;
             }
         }
         List<Client> clients = wareFlow.getClients();
         for (Client client : clients) {
-            if (client.getUsername() == username || message.isEmpty()) {
-                message += "This username is already taken. ";
+            if (client.getUsername().equals(username) || (message.isEmpty())) {
+                message.append("This username is already taken. ");
                 break;
             }
         }
@@ -153,18 +149,18 @@ public class UserController {
             //Employee nEmployee = new Employee(username, name, password, phoneNumber, wareFlow);
             if (message.isEmpty()) {
                 wareFlow.addEmployee(username, name, password, phoneNumber);
-                return "The employee has been sucessfully added!";
+                return "The employee has been successfully added!";
             }
         } else {
-            if (address == "" || address == null) {
-                message += "The address must not be empty. ";
+            if (address.isEmpty()) {
+                message.append("The address must not be empty. ");
             }
             if (message.isEmpty()) {
                 wareFlow.addClient(username, name, password, phoneNumber, address);
-                return "The client has been sucessfully added!";
+                return "The client has been successfully added!";
             }
         }
-        return message;
+        return message.toString();
     }
 
 
@@ -174,61 +170,59 @@ public class UserController {
      * @param newName        the new name to be updated in the employee or client's account
      * @param newPhoneNumber the new phone number to be updated in the employee or client's account
      * @param newAddress     if the account is a client account, the new address to be updated in the client's account
-     * @return Returns if the information associated to an employee or a client has been sucessfully updated in the WareFlow Application.
+     * @return Returns if the information associated to an employee or a client has been successfully updated in the WareFlow Application.
      * @author Yvehenry Julsain
      * This method is used to update the information associated to an employee or a client account within the WareFlow application.
      */
 
     public static String updateEmployeeOrClient(String username, String newPassword, String newName, String newPhoneNumber, String newAddress) { //TODO
 
-        String message = "";
-        if (username == "manager") {
-            message += "The username cannot be 'manager'. ";
+        StringBuilder message = new StringBuilder();
+        if (username.equals("manager")) {
+            message.append("The username cannot be 'manager'. ");
         }
-        if (username == "" || username == null) {
-            message += "The username must not be empty. ";
+        if (username.isEmpty()) {
+            message.append("The username must not be empty. ");
         }
-        if (!(username == null || username.isEmpty())) {
+        if (!username.isEmpty()) {
             char[] usernameArray = username.toCharArray();
             for (char letter : usernameArray) {
                 if (!("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890 ".contains(String.valueOf(letter)))) {
-                    message += "The username contains an invalid character. The username can only contain letters from a to z, letters from A to Z, and numbers from 0 to 9. ";
+                    message.append("The username contains an invalid character. The username can only contain letters from a to z, letters from A to Z, and numbers from 0 to 9. ");
                 }
             }
         }
 
         List<Employee> employees = wareFlow.getEmployees();
         for (Employee employee : employees) {
-            if (employee.getUsername() == username) {
+            if (employee.getUsername().equals(username)) {
                 if (!(newPassword == null || newPassword.isEmpty())) {
-                    if (!(newPassword == "" || newPassword == null)) {
-                        if (newPassword.length() < 4) {
-                            message += "Password must be at least four characters long. ";
+                    if (newPassword.length() < 4) {
+                        message.append("Password must be at least four characters long. ");
+                    }
+                    if (!(newPassword.contains("!") || newPassword.contains("#") || newPassword.contains("$"))) {
+                        message.append("Password must contain a special character out of <!#$> ");
+                    }
+                    char[] passwordArray = newPassword.toCharArray();
+                    boolean containsUppercase = false;
+                    boolean containsLowercase = false;
+                    for (char letter : passwordArray) {
+                        if (Character.isUpperCase(letter)) {
+                            containsUppercase = true;
+                            break;
                         }
-                        if (!(newPassword.contains("!") || newPassword.contains("#") || newPassword.contains("$"))) {
-                            message += "Password must contain a special character out of <!#$> ";
+                    }
+                    for (char letter : passwordArray) {
+                        if (Character.isLowerCase(letter)) {
+                            containsLowercase = true;
+                            break;
                         }
-                        char[] passwordArray = newPassword.toCharArray();
-                        boolean containsUppercase = false;
-                        boolean containsLowercase = false;
-                        for (char letter : passwordArray) {
-                            if (Character.isUpperCase(letter)) {
-                                containsUppercase = true;
-                                break;
-                            }
-                        }
-                        for (char letter : passwordArray) {
-                            if (Character.isLowerCase(letter)) {
-                                containsLowercase = true;
-                                break;
-                            }
-                        }
-                        if (!containsUppercase) {
-                            message += "Password must contain an upper case character. ";
-                        }
-                        if (!containsLowercase) {
-                            message += "Password must contain an lower case character. ";
-                        }
+                    }
+                    if (!containsUppercase) {
+                        message.append("Password must contain an upper case character. ");
+                    }
+                    if (!containsLowercase) {
+                        message.append("Password must contain an lower case character. ");
                     }
                     if (message.isEmpty()) employee.setPassword(newPassword);
                 }
@@ -238,41 +232,39 @@ public class UserController {
                 if (!(newPhoneNumber.isEmpty())) {
                     employee.setPhoneNumber(newPhoneNumber);
                 }
-                if (message.isEmpty()) return "Employee account information sucessfully updated.";
+                if (message.isEmpty()) return "Employee account information successfully updated.";
             }
         }
         List<Client> clients = wareFlow.getClients();
         for (Client client : clients) {
-            if (client.getUsername() == username) {
+            if (client.getUsername().equals(username)) {
                 if (!(newPassword == null || newPassword.isEmpty())) {
-                    if (!(newPassword == "" || newPassword == null)) {
-                        if (newPassword.length() < 4) {
-                            message += "Password must be at least four characters long. ";
+                    if (newPassword.length() < 4) {
+                        message.append("Password must be at least four characters long. ");
+                    }
+                    if (!(newPassword.contains("!") || newPassword.contains("#") || newPassword.contains("$"))) {
+                        message.append("Password must contain a special character out of <!#$> ");
+                    }
+                    char[] passwordArray = newPassword.toCharArray();
+                    boolean containsUppercase = false;
+                    boolean containsLowercase = false;
+                    for (char letter : passwordArray) {
+                        if (Character.isUpperCase(letter)) {
+                            containsUppercase = true;
+                            break;
                         }
-                        if (!(newPassword.contains("!") || newPassword.contains("#") || newPassword.contains("$"))) {
-                            message += "Password must contain a special character out of <!#$> ";
+                    }
+                    for (char letter : passwordArray) {
+                        if (Character.isLowerCase(letter)) {
+                            containsLowercase = true;
+                            break;
                         }
-                        char[] passwordArray = newPassword.toCharArray();
-                        boolean containsUppercase = false;
-                        boolean containsLowercase = false;
-                        for (char letter : passwordArray) {
-                            if (Character.isUpperCase(letter)) {
-                                containsUppercase = true;
-                                break;
-                            }
-                        }
-                        for (char letter : passwordArray) {
-                            if (Character.isLowerCase(letter)) {
-                                containsLowercase = true;
-                                break;
-                            }
-                        }
-                        if (!containsUppercase) {
-                            message += "Password must contain an upper case character. ";
-                        }
-                        if (!containsLowercase) {
-                            message += "Password must contain an lower case character. ";
-                        }
+                    }
+                    if (!containsUppercase) {
+                        message.append("Password must contain an upper case character. ");
+                    }
+                    if (!containsLowercase) {
+                        message.append("Password must contain an lower case character. ");
                     }
                     if (message.isEmpty()) client.setPassword(newPassword);
                 }
@@ -285,7 +277,7 @@ public class UserController {
                 if (!(newAddress.isEmpty())) {
                     client.setAddress(newAddress);
                 }
-                if (message.isEmpty()) return "Client account information sucessfully updated.";
+                if (message.isEmpty()) return "Client account information successfully updated.";
             }
         }
         return "The account was not found in the WareFlow application.";
