f75d79254a0ceb72d8a69b77783beecdbfb802a7
neeshal <neeshalimrit@outlook.com>
2024-04-06 14:51:22 -0400

    Added 2 new pages

---
+++ src/main/java/ca/mcgill/ecse/wareflow/javafx/fxml/controllers/UsersPageController.java
@@ -0,0 +1,19 @@
+package ca.mcgill.ecse.wareflow.javafx.fxml.controllers;
+
+
+import java.sql.Date;
+import ca.mcgill.ecse.wareflow.controller.*;
+import ca.mcgill.ecse.wareflow.javafx.fxml.WareFlowFxmlView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ChoiceBox;
+
+
+
+public class UsersPageController {
+    
+}
